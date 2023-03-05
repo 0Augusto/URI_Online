fad5a2c6fa4d6908c281baa9642034e766fba4b7
Henrique Augusto <73003917+0Augusto@users.noreply.github.com>
2023-03-05 16:15:24 -0300

    Add files via upload

---
+++ BEGINNER/CPlusPlus/1044.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B = 0;
+
+    cin >> A >> B;
+
+    if (A % B == 0 || B % A == 0){
+        cout << "Sao Multiplos" << endl;
+    }
+    else {
+        cout << "Nao sao Multiplos" << endl;
+    }
+
+return 0;
+}
